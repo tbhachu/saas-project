1e5ae215c452e77bf5ffcc2c3e2d379eb462a1c6
tbhachu <sem.b1901@gmail.com>
2022-09-29 15:18:25 -0700

    Initial commit

---
+++ src/main/java/edu/greenriver/edu/saasproject/SaasProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.edu.saasproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/greenriver/edu/saasproject/SaasProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.edu.saasproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
