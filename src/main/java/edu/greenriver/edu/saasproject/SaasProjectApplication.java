e7174b372ddd0169e15dea0221e606d98a2ea007
tbhachu <sem.b1901@gmail.com>
2022-09-30 16:10:53 -0700

    Added thymeleaf dependency to display html pages from templates folder.

--- src/main/java/edu/greenriver/edu/saasproject/SaasProjectApplication.java
+++ src/main/java/edu/greenriver/edu/saasproject/SaasProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SaasProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SaasProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(SaasProjectApplication.class, args);
+    }
 }
