17b268f819980601285035f3ba5f2cfcbcd73f98
tbhachu <sem.b1901@gmail.com>
2022-10-13 21:35:43 -0700

    Renamed Instrument.java to StringInstrument.java.

---
+++ src/main/java/edu/greenriver/edu/saasproject/models/StringInstrument.java
@@ -0,0 +1,14 @@
+package edu.greenriver.edu.saasproject.models;
+
+public class StringInstrument
+{
+    private String name;
+    private String material;
+    private boolean instrumentCase;
+    private boolean extraBow;
+    private boolean rosin;
+    private boolean extraStrings;
+
+
+
+}
