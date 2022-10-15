b1fe6f30c2bacdeed17848e704c7cea1376ffc8d
tbhachu <sem.b1901@gmail.com>
2022-10-14 21:18:44 -0700

    Created WebApiInstrument.java to write RESTful methods and error handling.

---
+++ src/main/java/edu/greenriver/edu/saasproject/controllers/WebApiInstrument.java
@@ -0,0 +1,98 @@
+package edu.greenriver.edu.saasproject.controllers;
+import edu.greenriver.edu.saasproject.models.Instrument;
+import edu.greenriver.edu.saasproject.models.Query;
+import edu.greenriver.edu.saasproject.services.InstrumentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/instrument")
+public class WebApiInstrument
+{
+    @RequestMapping("/home")
+    public String home()
+    {
+        return "index";
+    }
+
+    private InstrumentService service;
+
+    public WebApiInstrument(InstrumentService service)
+    {
+        this.service = service;
+    }
+
+    //GET request to http://localhost:8081/api/v1/instrument
+    @GetMapping("")
+    public ResponseEntity<List<Instrument>> allInstruments()
+    {
+        //return the response using a constructor
+        return new ResponseEntity<>(service.allInstruments(), HttpStatus.OK);
+    }
+
+    // how do we get inputs through a request
+    // **************************************
+
+    @GetMapping("query")
+    public ResponseEntity<Object> filterInstruments(@RequestBody Query query)
+    {
+        //we won't allow this end-point to be used with an empty query
+        if (query.getQueryValue() == null || query.getQueryValue().isEmpty())
+        {
+            return new ResponseEntity<>("The query string cannot be empty/null", HttpStatus.BAD_REQUEST);
+        }
+
+        //an alternative using factory methods
+        return ResponseEntity.ok(service.searchInstruments(query.getQueryValue()));
+    }
+
+    // **************************************
+
+    //POST request to http://localhost:8081/api/v1/instrument
+    @PostMapping("")
+    public ResponseEntity<Object> addInstrument(@RequestBody Instrument tempInstrument)
+    {
+        //don't add an empty joke
+        if (tempInstrument.getName() == null || tempInstrument.getName().isEmpty())
+        {
+            return new ResponseEntity<>("The instrument name cannot be empty/null", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(service.addInstrument(tempInstrument.getName(), tempInstrument.getType(),
+                tempInstrument.getMaterial(), tempInstrument.isCarryBag()), HttpStatus.CREATED);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Object> editInstrument(@RequestBody Instrument tempInstrument)
+    {
+        //make sure the id of the Raag is found
+        if (!service.instrumentExists(tempInstrument.getInstrumentID()))
+        {
+            return new ResponseEntity<>("Instrument does not exist!", HttpStatus.NOT_FOUND);
+        }
+        //don't add an empty joke
+        else if (tempInstrument.getName() == null || tempInstrument.getName().isEmpty())
+        {
+            return new ResponseEntity<>("The instrument name cannot be empty/null", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(service.updateInstrument(tempInstrument.getInstrumentID(),
+                tempInstrument.getName()), HttpStatus.OK);
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity<Object> deleteInstrument(@RequestBody Instrument tempInstrument)
+    {
+        // make sure the id of the Instrument is found
+        if (!service.instrumentExists(tempInstrument.getInstrumentID()))
+        {
+            return new ResponseEntity<>("Instrument does not exist!", HttpStatus.NOT_FOUND);
+        }
+
+        service.deleteInstrument(tempInstrument.getInstrumentID());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
