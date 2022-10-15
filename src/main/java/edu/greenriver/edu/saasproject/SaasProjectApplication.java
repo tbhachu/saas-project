9c21575ff313bf97dcde054f53a7abcf2e13f4e3
tbhachu <sem.b1901@gmail.com>
2022-10-14 18:50:10 -0700

    Added method to test CRUD applications from service class.

--- src/main/java/edu/greenriver/edu/saasproject/SaasProjectApplication.java
+++ src/main/java/edu/greenriver/edu/saasproject/SaasProjectApplication.java
@@ -1,7 +1,11 @@
 package edu.greenriver.edu.saasproject;
 
+import edu.greenriver.edu.saasproject.models.Raag;
+import edu.greenriver.edu.saasproject.services.RaagService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class SaasProjectApplication {
@@ -9,5 +13,13 @@ public class SaasProjectApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SaasProjectApplication.class, args);
+
+        RaagService raags = new RaagService();
+
+        raags.addRaag("Kaafi", "Khamaj", "Evening",
+                "Sa Re ga ma Pa Da ni Sa", "Sa ni Da Pa ma g Re Sa");
+
+
+        System.out.println(raags);
     }
 }
