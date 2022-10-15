ff8e87a4d49836cc69d340f7cf6af0dadf819bad
tbhachu <sem.b1901@gmail.com>
2022-10-14 19:16:59 -0700

    Edited aroh and avroh fields with vaadi paramater.

--- src/main/java/edu/greenriver/edu/saasproject/services/RaagService.java
+++ src/main/java/edu/greenriver/edu/saasproject/services/RaagService.java
@@ -12,22 +12,18 @@ import java.util.UUID;
 public class RaagService
 {
     private List<Raag> raags = new ArrayList<>(List.of(
-            new Raag("Kalyan", "Kalyaan", "Evening",
-                    "Sa Re Ga ma Pa Da Ni Sa", "Sa Ni Da Pa ma Ga Re Sa"),
-            new Raag("Bihag", "Bilaaval", "Night",
-                    "Sa Ga Ma Pa Ni Sa", "Sa Ni Pa Ga Ma Ga  Re Sa"),
-            new Raag("Desh", "Khamaj", "Morning",
-                    "Na Sa Re Ma Pa Ni Sa", "Sa ni Dha Pa Da Ma Ga Re Pa Ma Ga Re Ga Ni Sa"),
-            new Raag("Maajh", "Khamaj", "Night",
-                    "Sa Re Ma Pa Ni Sa", "Sa ni Da Pa Da Ma Ga Ma Re Pa ga Re Ni Sa")
+            new Raag("Kalyan", "Kalyaan", "Evening", "Ga"),
+            new Raag("Bihag", "Bilaaval", "Night", "Ga"),
+            new Raag("Desh", "Khamaj", "Morning", "Re"),
+            new Raag("Maajh", "Khamaj", "Night", "Ma")
 
     ));
 
     //CREATE
     public Raag addRaag(String raagName, String thaat,
-                        String time, String aroh, String avroh)
+                        String time, String vaadi)
     {
-        Raag added = new Raag(raagName, thaat, time, aroh, avroh);
+        Raag added = new Raag(raagName, thaat, time, vaadi);
         raags.add(added);
         return added;
     }
