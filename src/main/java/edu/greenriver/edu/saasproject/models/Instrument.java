fda9a0d1912750d0c010aa2e14c429bb983ff50c
tbhachu <sem.b1901@gmail.com>
2022-10-14 19:05:53 -0700

    Renamed class StringInstrument.java to Instrument.java and added constructor and getters/setters.

---
+++ src/main/java/edu/greenriver/edu/saasproject/models/Instrument.java
@@ -0,0 +1,67 @@
+package edu.greenriver.edu.saasproject.models;
+
+import java.util.UUID;
+
+public class Instrument
+{
+    private UUID instrumentID;
+    private String name;
+    private String type;
+    private String material;
+    private boolean carryBag;
+
+    public Instrument(String name, String type, String material, boolean carryBag) {
+        instrumentID = UUID.randomUUID();
+        this.name = name;
+        this.type = type;
+        this.material = material;
+        this.carryBag = carryBag;
+    }
+
+    public UUID getInstrumentID() {
+        return instrumentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public boolean isCarryBag() {
+        return carryBag;
+    }
+
+    public void setCarryBag(boolean carryBag) {
+        this.carryBag = carryBag;
+    }
+
+    @Override
+    public String toString() {
+        return "Instrument{" +
+                "instrumentID=" + instrumentID +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", material='" + material + '\'' +
+                ", carryBag=" + carryBag +
+                '}';
+    }
+}
