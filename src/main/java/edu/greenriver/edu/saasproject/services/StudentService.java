e6dca063dfdc5085e02e6b9565547c261814eb6e
tbhachu <sem.b1901@gmail.com>
2022-11-01 00:58:56 -0700

    Created StudentService.java to support CRUD methods.

---
+++ src/main/java/edu/greenriver/edu/saasproject/services/StudentService.java
@@ -0,0 +1,88 @@
+package edu.greenriver.edu.saasproject.services;
+
+import edu.greenriver.edu.saasproject.models.Raag;
+import edu.greenriver.edu.saasproject.models.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class StudentService
+{
+
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student("None", "N/A", "N/A", "N/A")
+    ));
+
+    //CREATE
+    public Student addStudent(String studentFName, String studentLName,
+                           String studentRaag, String studentInstrument)
+    {
+        Student added = new Student(studentFName, studentLName, studentRaag, studentInstrument);
+        students.add(added);
+        return added;
+    }
+
+    //READ
+    public List<Student> allStudents()
+    {
+        return students;
+    }
+
+    public List<Student> searchStudents(String queryValue)
+    {
+        return students.stream()
+                .filter(student -> student.getStudentLName().toLowerCase()
+                        .contains(queryValue.toLowerCase()))
+                .toList();
+    }
+
+    //UPDATE
+    public Student updateStudent(UUID id, String studentFName, String studentLName,
+                           String studentRaag, String studentInstrument)
+    {
+        Optional<Student> foundStudent = students.stream()
+                .filter(student -> student.getStudentID().equals(id))
+                .findFirst();
+
+        if (foundStudent.isPresent())
+        {
+            //update it
+            Student student = foundStudent.get();
+            student.setStudentFName(studentFName);
+            student.setStudentLName(studentLName);
+            student.setStudentRaag(studentRaag);
+            student.setStudentInstrument(studentInstrument);
+            return student;
+        }
+        else
+        {
+            //otherwise, return null
+            return null;
+        }
+    }
+
+    //DELETE
+    public void deleteStudent(UUID id)
+    {
+        students = students.stream()
+                .filter(student -> !student.getStudentID().equals(id))
+                .toList();
+    }
+
+    public boolean studentExists(UUID id)
+    {
+        return students.stream()
+                .anyMatch(student -> student.getStudentID().equals(id));
+    }
+
+    @Override
+    public String toString() {
+        return "StudentService{" +
+                "students=" + students +
+                '}';
+    }
+}
